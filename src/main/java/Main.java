b2ca250b322cbac02ec980c794b0dc28dc747bb7
OptypID <drozhzhaai@gmail.com>
2024-02-06 00:15:03 +0300

    Invoice Calculator v.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int people = 0;
+        while (people < 2) {
+            System.out.println("На скольких человек необходимо разделить счёт?");
+            Scanner scanner = new Scanner(System.in);
+            try {
+                people = scanner.nextInt();
+                if (people < 2) {
+                    System.out.println("Ошибка: неверное значение. Попробуйте снова");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Ошибка: неверное значение. Попробуйте снова");
+            }
+        }
+        Calculator.calculate(people);
     }
 }
